94fec7b18c7691c31ef7def298e9582fd5332203
romue404 <robert.mueller1990@googlemail.com>
2017-01-23 22:01:29 +0100

    add constants and project structure

---
+++ constants.py
@@ -0,0 +1,19 @@
+"""
+all configurable parameters of this game
+"""
+
+"""=====FPS====="""
+FPS = 60
+
+"""=====WINDOW====="""
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+"""=====BAR====="""
+BAR_WIDTH = 0.05 * WINDOW_WIDTH
+BAR_HEIGHT = 0.25 * WINDOW_HEIGHT
+
+"""=====COLORS====="""
+BACKGROUND_COLOR = (29,29,29)
+BAR_COLOR = (76, 175, 80)
+BALL_COLOR = (255, 235, 59)
